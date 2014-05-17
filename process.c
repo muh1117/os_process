3267990e3559a9a5a11cefe279c87d0c83755b50
muh1117 <muh1117@naver.com>
2014-05-17 09:04:03 +0900

    second upload process.c print 12child....

--- process.c
+++ process.c
@@ -5,40 +5,47 @@
 //#include "hanoi.h"
 
 int count = 0;
+//int child_count = 0;
 
 int main(){
 	pid_t fork_ret;
 	pid_t child;
 //	int local_val = 0;
 	int state;
+	int child_count = 0;
 
-	fork_ret = fork();
-	fork_ret = fork();
-	fork_ret = fork();
-	fork_ret = fork();
+	for(child_count =0; child_count < 5; ++child_count){
+//	while(child_count==3){
+		fork_ret = fork();
+//		fork_ret = fork();
+//		fork_ret = fork();
+//		fork_ret = fork();
 
-	if(fork_ret < 0){
-		printf("fork() error\n");
-		exit(1);
-	}
-	else if(0==fork_ret){
+		if(fork_ret < 0){
+			printf("fork() error\n");
+			exit(1);
+		}
+		else if(0==fork_ret){
 //		global_val++;
 //		local_val++;
-		hanoi('A', 'B', 'C', 3);
-		printf("CHILD - PID : %d parent's PID : %d \n", getpid(), getpid());
-	}
-	else{
+			child_count++;
+			hanoi('A', 'B', 'C', 2);
+			printf("Child Num.: %d CHILD - PID : %d parent's PID : %d \n",child_count,  getpid(), getpid());
+		}
+		else{
 //		global_val = global_val + 5;
 //		local_val = local_val + 5;
-		printf("PARENT - PID : %d child's PID : %d \n", getpid(), fork_ret);
+			printf("PARENT - PID : %d child's PID : %d \n", getpid(), fork_ret);
 
-		child = wait(&state);
-		printf("\t Child PID = %d \n", child);
+			child = wait(&state);
+			printf("\t Child PID = %d \n", child);
 //		printf("\t return value = %d \n", WEXITSTATUS(state));
 
-//		sleep(10);
+//			sleep(10);
+
+//			printf("\t create %d child.\n", child_count);
+		}
 	}
-	
 //	printf("\t global_val : %d \n", global_val);
 //	printf("\t local_val : %d \n", local_val);
 
